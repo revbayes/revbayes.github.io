a17a6107898c9d98f459cb7cb76f3684084c9c09
Walker Pett <will.pett@gmail.com>
2018-02-28 17:16:33 -0600

    rebuilding site

--- assets/js/lesson.js
+++ assets/js/lesson.js
@@ -1,9 +1,14 @@
+$(window).load(function() {
+  // When the page has loaded
+  $("body").show(0);
+});
+
 // Make all tables striped by default.
 $("table").addClass("table table-striped");
 
 
 // Handle foldable challenges and solutions (on click and at start).
-$(".challenge,.discussion,.solution").click(function(event) {
+$(".challenge,.discussion,.solution,.tutorial_files,.overview").click(function(event) {
     var trigger = $(event.target).has(".fold-unfold").size() > 0
                || $(event.target).filter(".fold-unfold").size() > 0;
     if (trigger) {
@@ -18,6 +23,115 @@ $(".challenge,.discussion,.solution").each(function() {
     h2.append("<span class='fold-unfold glyphicon glyphicon-collapse-down'></span>");
 });
 
+
+// Create an array of all script files and their contents
+var scripts = {};
+var _code = document.querySelectorAll('pre');
+for (var i = 0, element; element = _code[i]; i++) {
+  var output = element.parentElement.parentElement.getAttribute("output");
+  if( typeof scripts[output] == 'undefined' )
+    scripts[output] = "";
+  scripts[output] += element.firstChild.innerHTML;
+}
+
+// Retrieve script file for download
+function get_script(script) {
+  var blob = new Blob([scripts[script]], {type: "text/plain;charset=utf-8"});
+  saveAs(blob, script);
+}
+
+// Zip all files for download
+function get_files() {
+
+  var path = window.location.pathname.split('/');
+  var name = "revbayes_"+path[path.length-2];
+
+  var zip = new JSZip().folder(name);
+
+  var s = zip.folder("scripts");
+  for(var script in scripts) {
+    if( script != "null") {
+      s.file(script, scripts[script]);
+    }
+  }
+
+  var ul = document.getElementById("data_files");
+
+  var d = zip.folder("data");
+  var _li = ul.getElementsByTagName("li");
+  for (var i = 0; i < _li.length; ++i) {
+    var file = _li[i].firstChild.innerHTML;
+    d.file(file, $.get(file));
+  }
+  zip.generateAsync({type:"blob"})
+    .then(function(content) {
+      // see FileSaver.js
+      saveAs(content, name+".zip");
+  });
+}
+
+// Handle downloadable data files and scripts (on click and at start).
+$(".tutorial_files").each(function() {
+    var h2 = $("h2:first", this);
+    h2.prepend("<span onclick=get_files() title=\"Download Zip Archive\" class='download_files glyphicon glyphicon-download'></span>");
+    //h2.append("<span class='fold-unfold glyphicon glyphicon-collapse-down'></span>");
+
+    var ul = document.createElement('ul');
+    var i = 0;
+    for(var script in scripts) {
+      if( script != "null") {
+        ul.innerHTML += "<li><a onclick=get_script(\""+script+"\") onmouseover=\"\" style=\"cursor: pointer;\">"+script+"</a></li>";
+        i++;
+      }
+    }
+
+    if( i > 0 ) {
+      var row = document.createElement('div');
+      row.className = 'row';
+
+      var col = document.createElement('div');
+      col.className = 'col-md-9';
+
+      col.innerHTML += "<strong>Scripts</strong>";
+
+      col.appendChild(ul);
+      row.appendChild(col);
+      this.appendChild(row);
+
+      //$(">*:not(h2)", this).toggle();
+    }
+    else {
+      this.outerHTML = "";
+    }
+});
+
+// Add section hyperlinks
+$(".overview").each(function() {
+    var h2 = $("h2:first", this);
+    //h2.append("<span class='fold-unfold glyphicon glyphicon-collapse-down'></span>");
+
+    var _sections = document.getElementsByClassName('section');
+    
+    if( _sections.length > 0 ) {
+      var row = document.createElement('div');
+      row.className = 'row';
+
+      var col = document.createElement('div');
+      col.className = 'col-md-9';
+
+      col.innerHTML += "<strong>Sections</strong>";
+
+      var ul = document.createElement('ul');
+      for (var i = 0, element; element = _sections[i]; i++) {
+        element.id = "section"+i;
+        ul.innerHTML += "<li><a href=\"#"+element.id+"\">"+element.innerHTML+"</a></li>";
+      }
+
+      col.appendChild(ul);
+      row.appendChild(col);
+      this.appendChild(row);
+    }
+});
 
 // Handle searches.
 // Relies on document having 'meta' element with name 'search-domain'.
