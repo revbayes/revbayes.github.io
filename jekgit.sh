65d5b1238f366a964ec1077ae324993b9776b103
Walker Pett <will.pett@gmail.com>
2018-03-03 10:42:07 -0600

    modifying jekgit.sh

--- jekgit.sh
+++ jekgit.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
- 
-if [[ -z "$1" ]]; then
-  echo "Please enter a git commit message"
-  exit
+
+if git diff --exit-code > /dev/null
+then
+	git pull
+
+	msg=`git log -1 --pretty=%B`
+
+	cd _site && git reset --hard && git pull -f
+	cd ..
+	bundle exec jekyll build && \
+	cd _site
+
+	if git diff --exit-code > /dev/null
+	then
+		echo "Nothing to update on master."
+	else
+		echo "$msg"
+		echo "Successfully built and pushed to master."
+		# git add . && \
+		# git commit -am "$msg" && \
+		# git push origin master
+	fi
+	cd ..
+else
+	echo "Error: Source changes not staged for commit.\nPlease commit to source before updating master."
 fi
-  
-  cd _site && git reset --hard
-  git pull -f
-  cd ..
-bundle exec jekyll build && \
-  cd _site && \
-  git add . && \
-  git commit -am "$1" && \
-  git push origin master && \
-  cd .. && \
-  echo "Successfully built and pushed to GitHub."
