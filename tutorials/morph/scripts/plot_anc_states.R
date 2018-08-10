49dc2b7141d278de1b283022a8c44a03e10cf600
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2018-08-10 08:56:42 +0200

    Examples for the dnds tutorial

---
+++ tutorials/morph/scripts/plot_anc_states.R
@@ -0,0 +1,47 @@
+################################################################################
+#
+# Plot ancestral placental types inferred using two CTMC models of character evolution.
+#
+#
+# authors: Sebastian Hoehna, Will Freyman
+#
+################################################################################
+
+# if you do not have RevGadgets installed, then follow these instructions
+#library(devtools)
+#install_github("revbayes/RevGadgets")
+
+library(RevGadgets)
+
+tree_file = "output/ase_mk.tree"
+g <- plot_ancestral_states(tree_file, summary_statistic="MAP",
+                      tip_label_size=1,
+                      xlim_visible=NULL,
+                      node_label_size=0,
+                      show_posterior_legend=TRUE,
+                      node_size_range=c(1, 3),
+                      alpha=0.75)
+
+ggsave("Mammals_ASE_MK.pdf", g, width = 11, height = 9)
+
+tree_file = "output/ase_freeK.tree"
+g <- plot_ancestral_states(tree_file, summary_statistic="MAP",
+                      tip_label_size=1,
+                      xlim_visible=NULL,
+                      node_label_size=0,
+                      show_posterior_legend=TRUE,
+                      node_size_range=c(1, 3),
+                      alpha=0.75)
+
+ggsave("Mammals_ASE_FreeK.pdf", g, width = 11, height = 9)
+
+tree_file = "output/ase_freeK_RJ.tree"
+g <- plot_ancestral_states(tree_file, summary_statistic="MAP",
+                      tip_label_size=1,
+                      xlim_visible=NULL,
+                      node_label_size=0,
+                      show_posterior_legend=TRUE,
+                      node_size_range=c(1, 3),
+                      alpha=0.75)
+
+ggsave("Mammals_ASE_FreeK_RJ.pdf", g, width = 11, height = 9)
