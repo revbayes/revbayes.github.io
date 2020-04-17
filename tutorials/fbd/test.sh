0a14dafb42356b5865e090fdf328513c458f1efc
Walker Pett <will.pett@gmail.com>
2020-04-17 11:24:13 -0500

    update fbd test script

--- tutorials/fbd/test.sh
+++ tutorials/fbd/test.sh
@@ -1,5 +1,4 @@
 sed -i.bak 's/generations=10000/generations=1/g' scripts/mcmc_CEFBDP_Specimens.Rev
 ${rb_exec} -b scripts/mcmc_CEFBDP_Specimens.Rev
-if [ ! -d "output" ]; then
-     exit 1
-fi
+mv scripts/mcmc_CEFBDP_Specimens.Rev.bak scripts/mcmc_CEFBDP_Specimens.Rev
+rm -rf output
