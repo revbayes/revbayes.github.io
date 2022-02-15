ac1b9c49368450cdff8594f46c2d7c8a37ec28ee
Sebastian Hoehna <sebastian.hoehna@gmail.com>
2022-02-15 18:51:44 +0100

    using revgadgets now

--- tutorials/divrate/scripts/plot_EBD.R
+++ tutorials/divrate/scripts/plot_EBD.R
@@ -3,17 +3,24 @@
 #install_github("revbayes/RevGadgets")
 
 library(RevGadgets)
+library(ggplot2)
 
-tree <- read.nexus("data/primates_tree.nex")
+# specify the output files
+speciation_time_file <- "output/primates_EBD_speciation_times.log"
+speciation_rate_file <- "output/primates_EBD_speciation_rates.log"
+extinction_time_file <- "output/primates_EBD_extinction_times.log"
+extinction_rate_file <- "output/primates_EBD_extinction_rates.log"
 
-rev_out <- rev.process.div.rates(speciation_times_file = "output/primates_EBD_speciation_times.log",
-                                 speciation_rates_file = "output/primates_EBD_speciation_rates.log",
-                                 extinction_times_file = "output/primates_EBD_extinction_times.log",
-                                 extinction_rates_file = "output/primates_EBD_extinction_rates.log",
-                                 tree=tree,
-                                 burnin=0.25,numIntervals=1000)
+# read in and process rates
+rates <- processDivRates(speciation_time_log = speciation_time_file,
+                         speciation_rate_log = speciation_rate_file,
+                         extinction_time_log = extinction_time_file,
+                         extinction_rate_log = extinction_rate_file,
+                         burnin = 0.25)
 
-pdf("EBD.pdf")
-par(mfrow=c(2,2))
-rev.plot.div.rates(rev_out,use.geoscale=FALSE)
-dev.off()
+# plot rates through time
+p <- plotDivRates(df = rates) +
+        xlab("Millions of years ago") +
+        ylab("Rate per million years")
+
+ggsave("EBD.png", p)
