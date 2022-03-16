4f6b71b563412d486825589ecef4518c9c0d015d
rbillenstein <ronja@billenstein.com>
2022-03-16 16:04:18 +0100

    minor corrections

--- tutorials/coalescent/scripts/plotting_popsizes.R
+++ tutorials/coalescent/scripts/plotting_popsizes.R
@@ -11,112 +11,112 @@ summary = "median"
 ####################
 
 # constant
-population_size_log = "../output/horses_constant_NE.log"
+population_size_log = "../output/horses_iso_constant_NE.log"
 df <- processPopSizes(population_size_log, method = "constant", burnin = burnin, probs = probs, summary = summary)
 p <- plotPopSizes(df, method = "constant")# + ggplot2::coord_cartesian(ylim = c(1e3, 1e8), xlim = c(1e5, 0))
-ggplot2::ggsave("../figures/horses_constant.png", p)
+ggplot2::ggsave("../figures/horses_iso_constant.png", p)
 
 # skyline
-population_size_log = "../output/horses_skyline_NEs.log"
-interval_change_points_log = "../output/horses_skyline_times.log"
+population_size_log = "../output/horses_iso_skyline_NEs.log"
+interval_change_points_log = "../output/horses_iso_skyline_times.log"
 df <- processPopSizes(population_size_log, interval_change_points_log, method = "events", burnin = burnin, probs = probs, summary = summary)
 p <- plotPopSizes(df) + ggplot2::coord_cartesian(ylim = c(1e3, 1e8))
-ggplot2::ggsave("../figures/horses_skyline.png", p)
+ggplot2::ggsave("../figures/horses_iso_skyline.png", p)
 
 # skyline, MAP tree based (MAP tree from constant analysis)
-population_size_log = "../output/horses_skyline_maptreebased_NEs.log"
-interval_change_points_log = "../output/horses_skyline_maptreebased_times.log"
+population_size_log = "../output/horses_iso_skyline_maptreebased_NEs.log"
+interval_change_points_log = "../output/horses_iso_skyline_maptreebased_times.log"
 df <- processPopSizes(population_size_log, interval_change_points_log, method = "events", burnin = burnin, probs = probs, summary = summary)
 p <- plotPopSizes(df) + ggplot2::coord_cartesian(ylim = c(1e3, 1e8))
-ggplot2::ggsave("../figures/horses_skyline_maptreebased.png", p)
+ggplot2::ggsave("../figures/horses_iso_skyline_maptreebased.png", p)
 
 # GMRF, 10 intervals
-population_size_log = "../output/horses_GMRF_NEs.log"
-interval_change_points_log = "../output/horses_GMRF_times.log"
+population_size_log = "../output/horse_isos_GMRF_NEs.log"
+interval_change_points_log = "../output/horses_iso_GMRF_times.log"
 df <- processPopSizes(population_size_log, interval_change_points_log, method = "specified", burnin = burnin, probs = probs, summary = summary)
 p <- plotPopSizes(df, method = "specified") + ggplot2::coord_cartesian(ylim = c(1e3, 1e8))
-ggplot2::ggsave("../figures/horses_GMRF.png", p)
+ggplot2::ggsave("../figures/horses_iso_GMRF.png", p)
 
 # GMRF, 100 intervals
-population_size_log = "../output/horses_GMRF_100_NEs.log"
-interval_change_points_log = "../output/horses_GMRF_100_times.log"
+population_size_log = "../output/horses_iso_GMRF_100_NEs.log"
+interval_change_points_log = "../output/horses_iso_GMRF_100_times.log"
 df <- processPopSizes(population_size_log, interval_change_points_log, method = "specified", summary = "median", burnin = 0.1)
 p <- plotPopSizes(df, method = "specified") + ggplot2::coord_cartesian(ylim = c(1e3, 1e8))
-ggplot2::ggsave("../figures/horses_GMRF_100.png", p)
+ggplot2::ggsave("../figures/horses_iso_GMRF_100.png", p)
 
 # GMRF, MAP tree based, 10 intervals (MAP tree from constant analysis)
-population_size_log = "../output/horses_GMRF_maptreebased_NEs.log"
-interval_change_points_log = "../output/horses_GMRF_maptreebased_times.log"
+population_size_log = "../output/horses_iso_GMRF_maptreebased_NEs.log"
+interval_change_points_log = "../output/horses_iso_GMRF_maptreebased_times.log"
 df <- processPopSizes(population_size_log, interval_change_points_log, method = "specified", burnin = burnin, probs = probs, summary = summary)
 p <- plotPopSizes(df, method = "specified") + ggplot2::coord_cartesian(ylim = c(1e3, 1e8))
-ggplot2::ggsave("../figures/horses_GMRF_maptreebased.png", p)
+ggplot2::ggsave("../figures/horses_iso_GMRF_maptreebased.png", p)
 
 # GMRF, MAP tree based, 100 intervals (MAP tree from constant analysis)
-population_size_log = "../output/horses_GMRF_maptreebased_100_NEs.log"
-interval_change_points_log = "../output/horses_GMRF_maptreebased_100_times.log"
+population_size_log = "../output/horses_iso_GMRF_maptreebased_100_NEs.log"
+interval_change_points_log = "../output/horses_iso_GMRF_maptreebased_100_times.log"
 df <- processPopSizes(population_size_log, interval_change_points_log, method = "specified", burnin = burnin, probs = probs, summary = summary)
 p <- plotPopSizes(df, method = "specified") + ggplot2::coord_cartesian(ylim = c(1e3, 1e8))
-ggplot2::ggsave("../figures/horses_GMRF_maptreebased_100.png", p)
+ggplot2::ggsave("../figures/horses_iso_GMRF_maptreebased_100.png", p)
 
 # GMRF, tree based, 10 intervals (trees from constant analysis)
-population_size_log = "../output/horses_GMRF_treebased_NEs.log"
-interval_change_points_log = "../output/horses_GMRF_treebased_times.log"
+population_size_log = "../output/horses_iso_GMRF_treebased_NEs.log"
+interval_change_points_log = "../output/horses_iso_GMRF_treebased_times.log"
 df <- processPopSizes(population_size_log, interval_change_points_log, method = "specified", burnin = burnin, probs = probs, summary = summary)
 p <- plotPopSizes(df, method = "specified") + ggplot2::coord_cartesian(ylim = c(1e3, 1e8))
-ggplot2::ggsave("../figures/horses_GMRF_treebased.png", p)
+ggplot2::ggsave("../figures/horses_iso_GMRF_treebased.png", p)
 
 # HSMRF, 10 intervals
-population_size_log = "../output/horses_HSMRF_NEs.log"
-interval_change_points_log = "../output/horses_HSMRF_times.log"
+population_size_log = "../output/horses_iso_HSMRF_NEs.log"
+interval_change_points_log = "../output/horses_iso_HSMRF_times.log"
 df <- processPopSizes(population_size_log, interval_change_points_log, method = "specified", burnin = burnin, probs = probs, summary = summary)
 p <- plotPopSizes(df, method = "specified") + ggplot2::coord_cartesian(ylim = c(1e3, 1e8))
-ggplot2::ggsave("../figures/horses_HSMRF.png", p)
+ggplot2::ggsave("../figures/horses_iso_HSMRF.png", p)
 
 # HSMRF, 100 intervals
-population_size_log = "../output/horses_HSMRF_100_NEs.log"
-interval_change_points_log = "../output/horses_HSMRF_100_times.log"
+population_size_log = "../output/horses_iso_HSMRF_100_NEs.log"
+interval_change_points_log = "../output/horses_iso_HSMRF_100_times.log"
 df <- processPopSizes(population_size_log, interval_change_points_log, method = "specified", burnin = burnin, probs = probs, summary = summary)
 p <- plotPopSizes(df, method = "specified") + ggplot2::coord_cartesian(ylim = c(1e3, 1e8))
-ggplot2::ggsave("../figures/horses_HSMRF_100.png", p)
+ggplot2::ggsave("../figures/horses_iso_HSMRF_100.png", p)
 
 # HSMRF, MAP tree based, 10 intervals (MAP tree from constant analysis)
-population_size_log = "../output/horses_HSMRF_maptreebased_NEs.log"
-interval_change_points_log = "../output/horses_HSMRF_maptreebased_times.log"
+population_size_log = "../output/horses_iso_HSMRF_maptreebased_NEs.log"
+interval_change_points_log = "../output/horses_iso_HSMRF_maptreebased_times.log"
 df <- processPopSizes(population_size_log, interval_change_points_log, method = "specified", burnin = burnin, probs = probs, summary = summary)
 p <- plotPopSizes(df, method = "specified") + ggplot2::coord_cartesian(ylim = c(1e3, 1e8))
-ggplot2::ggsave("../figures/horses_HSMRF_maptreebased.png", p)
+ggplot2::ggsave("../figures/horses_iso_HSMRF_maptreebased.png", p)
 
 # HSMRF, MAP tree based, 100 intervals (MAP tree from constant analysis)
-population_size_log = "../output/horses_HSMRF_maptreebased_100_NEs.log"
-interval_change_points_log = "../output/horses_HSMRF_maptreebased_100_times.log"
+population_size_log = "../output/horses_iso_HSMRF_maptreebased_100_NEs.log"
+interval_change_points_log = "../output/horses_iso_HSMRF_maptreebased_100_times.log"
 df <- processPopSizes(population_size_log, interval_change_points_log, method = "specified", burnin = burnin, probs = probs, summary = summary)
 p <- plotPopSizes(df, method = "specified") + ggplot2::coord_cartesian(ylim = c(1e3, 1e8))
-ggplot2::ggsave("../figures/horses_HSMRF_maptreebased_100.png", p)
+ggplot2::ggsave("../figures/horses_iso_HSMRF_maptreebased_100.png", p)
 
 # CPP
-population_size_log = "../output/horses_CPP_NEs.log"
-interval_change_points_log = "../output/horses_CPP_times.log"
+population_size_log = "../output/horses_iso_CPP_NEs.log"
+interval_change_points_log = "../output/horses_iso_CPP_times.log"
 df <- processPopSizes(population_size_log, interval_change_points_log, method = "events", burnin = burnin, probs = probs, summary = summary)
 p <- plotPopSizes(df) + ggplot2::coord_cartesian(ylim = c(1e3, 1e8))
-ggplot2::ggsave("../figures/horses_CPP.png", p)
+ggplot2::ggsave("../figures/horses_iso_CPP.png", p)
 
 # CPP, MAP tree based (MAP tree from constant analysis)
-population_size_log = "../output/horses_CPP_maptreebased_NEs.log"
-interval_change_points_log = "../output/horses_CPP_maptreebased_times.log"
+population_size_log = "../output/horses_iso_CPP_maptreebased_NEs.log"
+interval_change_points_log = "../output/horses_iso_CPP_maptreebased_times.log"
 df <- processPopSizes(population_size_log, interval_change_points_log, method = "events", burnin = burnin, probs = probs, summary = summary)
 p <- plotPopSizes(df) + ggplot2::coord_cartesian(ylim = c(1e3, 1e8))
-ggplot2::ggsave("../figures/horses_CPP_maptreebased.png", p)
+ggplot2::ggsave("../figures/horses_iso_CPP_maptreebased.png", p)
 
 
 
 # piecewise, 3 constant
-population_size_log_skyline = "../output/horses_skyline_NEs.log"
-interval_change_points_log_skyline = "../output/horses_skyline_times.log"
+population_size_log_skyline = "../output/horses_iso_skyline_NEs.log"
+interval_change_points_log_skyline = "../output/horses_iso_skyline_times.log"
 df_skyline <- processPopSizes(population_size_log = population_size_log_skyline, interval_change_points_log = interval_change_points_log_skyline, method = "events", burnin = burnin, probs = probs, summary = summary)
 p_skyline <- plotPopSizes(df_skyline) + ggplot2::coord_cartesian(ylim = c(1e3, 1e8))
 
-population_size_log = "../output/horses_piecewise_3const_NEs.log"
-interval_change_points_log = "../output/horses_piecewise_3const_times.log"
+population_size_log = "../output/horses_iso_piecewise_3const_NEs.log"
+interval_change_points_log = "../output/horses_iso_piecewise_3const_times.log"
 pop_sizes <- readTrace(population_size_log, burnin = burnin)[[1]]
 interval_times <- readTrace(interval_change_points_log, burnin = burnin)[[1]]
 
@@ -134,16 +134,16 @@ df$time_end = c(time_medians, Inf)
 
 p <- p_skyline + ggplot2::geom_segment(data = df, aes(x = time, xend = time_end, y = value, yend = value), size = 0.9, color = "blue") +
   ggplot2::geom_rect(data = df, aes(xmin = time, xmax = time_end, ymin = lower, ymax = upper), fill = "blue", alpha = 0.2)
-ggplot2::ggsave("../figures/horses_piecewise_3const.png", p)
+ggplot2::ggsave("../figures/horses_iso_piecewise_3const.png", p)
 
 # piecewise, 6 different
-population_size_log_skyline = "../output/horses_skyline_NEs.log"
-interval_change_points_log_skyline = "../output/horses_skyline_times.log"
+population_size_log_skyline = "../output/horses_iso_skyline_NEs.log"
+interval_change_points_log_skyline = "../output/horses_iso_skyline_times.log"
 df_skyline <- processPopSizes(population_size_log = population_size_log_skyline, interval_change_points_log = interval_change_points_log_skyline, method = "events", burnin = burnin, probs = probs, summary = summary)
 p_skyline <- plotPopSizes(df_skyline) + ggplot2::coord_cartesian(ylim = c(1e3, 1e8))
 
-population_size_log = "../output/horses_piecewise_NEs.log"
-interval_change_points_log = "../output/horses_piecewise_times.log"
+population_size_log = "../output/horses_iso_piecewise_6diff_NEs.log"
+interval_change_points_log = "../output/horses_iso_piecewise_6diff_times.log"
 pop_sizes <- readTrace(population_size_log, burnin = burnin)[[1]]
 interval_times <- readTrace(interval_change_points_log, burnin = burnin)[[1]]
 
@@ -259,7 +259,7 @@ df$time <- grid
 p <- p_skyline +
   ggplot2::geom_line(data = df, ggplot2::aes(x = time, y = value), size = 0.9, color = "blue") +
   ggplot2::geom_ribbon(data = df, ggplot2::aes(x = time, ymin = lower, ymax = upper), fill = "blue", alpha = 0.2)
-ggplot2::ggsave("../figures/horses_piecewise_6diff.png", p)
+ggplot2::ggsave("../figures/horses_iso_piecewise_6diff.png", p)
 
 #######################
 # heterochronous data #
