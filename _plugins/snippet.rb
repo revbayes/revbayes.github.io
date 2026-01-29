e54f896ae5ce71b5ebdc355c7bcfc5970080a557
Benjamin Redelings <benjamin.redelings@gmail.com>
2026-01-29 14:27:41 +0200

    Make the snippet block ignore blank lines and comment lines.

--- _plugins/snippet.rb
+++ _plugins/snippet.rb
@@ -1,15 +1,3 @@
-def lines_match_in_order?(snippet_lines, file_lines)
-  i = 0
-  snippet_lines.each do |snippet_line|
-    while i < file_lines.length && file_lines[i].strip != snippet_line.strip
-      i += 1
-    end
-    return false if i == file_lines.length
-    i += 1
-  end
-  true
-end
-
 module Liquid
 	class Snippet < Block
 		def initialize(tag_name, markup, options)
@@ -18,9 +6,10 @@ module Liquid
 		end
 
 		def render(context)
-                        code_block = super
+                        code_block = super.strip
 
-                        code_lines = code_block.lines.map{ |l| l.strip }
+                        # Drop blank lines and comment lines
+                        code_lines = code_block.lines.filter_map{ |l| l.strip unless (l.strip.empty? or l.strip.start_with?('#'))}
 
                         # Site source directory (so we don't need absolute paths)
                         current_page_path = context.registers[:page]["path"]    # e.g., "docs/example.md"
@@ -29,24 +18,29 @@ module Liquid
                         # Resolve snippet file relative to the document
                         site_source = context.registers[:site].source
                         file_path = File.expand_path(File.join(site_source, page_dir, @filename))
-  
+
                         unless File.exist?(file_path)
                           raise IOError, "❌ Error: File '#{@filename}' not found at #{file_path}."
                         end
 
                         file_lines = File.readlines(file_path).map{ |l| l.strip }
-
+                        
                         # Search for exact match of code_lines in file_lines
-                        match_found = lines_match_in_order?(code_lines, file_lines)
-
-                        unless match_found
-                          raise RuntimeError, "❌ Error: Code block not found in file `#{@filename}`:\n\n#{code_block}"
+                        i = 0
+                        code_lines.each do |snippet_line|
+                          while i < file_lines.length && file_lines[i].strip != snippet_line.strip
+                            i += 1
+                          end
+                          if i == file_lines.length
+                            raise RuntimeError, "❌ Error: line for code block not found in file `#{@filename}`:\n\n#{snippet_line}\n\nin block:\n#{code_block}"
+                          end
+                          i += 1
                         end
-
+                        
                         <<~MARKDOWN
-
-                        #{code_block}
-
+                        ```
+                        #{code_block.strip}
+                        ```
                         MARKDOWN
 		end
 	end
